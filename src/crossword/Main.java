2ad561a2968709ad925ec86de000b7e99879c59f
Bogdan Mischie <bogdim08@outlook.com>
2024-06-10 08:09:22 -0700

    GU5JK9IE5

--- src/crossword/Main.java
+++ src/crossword/Main.java
@@ -9,8 +9,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main {
-	private static int h = 2;
-	private static int w = 2;
+	private static int h = 3;
+	private static int w = 3;
+	private static int i = 0;
+	private static int k = 0;
+	private static int temp = 0;
+	private static boolean b = false;
+	private static String attempt ="";
+	private static String secretWord ="aaa";
 	public static void main(String[] args) {
 		// Creating instance of JFrame
         JFrame frame = new JFrame("Word Serch");
@@ -47,8 +53,42 @@ public class Main {
 	        	tutorial.setBounds((int) screenSize.getWidth()/2-75, (int) screenSize.getHeight()/2 -200, 250 ,100 );
 	    		frame.add(tutorial);
 	    		tutorial.doClick();
+	    		tutorial.setEnabled(false);
 	    	
-	        	sizeMenu(frame);
+	        	
+	    		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();   
+	    		
+	    		JTextField selctorw = new JTextField("0");
+	    		JTextField selctorh = new JTextField("0");
+	    		JButton confirm = new JButton("Confirm size");
+	    		
+	    		selctorw.setBounds((int) screenSize.getWidth()/2 -50, (int) screenSize.getHeight()/2 -50, 100 ,100 );
+	    		selctorh.setBounds((int) screenSize.getWidth()/2 +50, (int) screenSize.getHeight()/2 -50, 100 ,100 );
+	    		
+	    		
+	            // adding button in JFrame
+	    		confirm.setBounds((int) screenSize.getWidth()/2-50, (int) screenSize.getHeight()/2 +50, 200 ,100 );
+	    		frame.add(confirm);
+	    		confirm.doClick();
+	    		
+	    		
+	            frame.add(selctorw);
+	            frame.add(selctorh);
+	            confirm.addActionListener(new ActionListener(){ 
+	    	        public void actionPerformed(ActionEvent e){ 
+	    	        	if(Integer.parseInt(selctorw.getText())>w) {
+	    	        		w = Integer.parseInt(selctorw.getText());
+	    	        	}
+	    	        	if(Integer.parseInt(selctorh.getText())>h) {
+	    	        		h = Integer.parseInt(selctorh.getText());
+	    	        	}
+	    	        	confirm.setVisible(false);
+	    	        	selctorw.setVisible(false);
+	    	        	selctorh.setVisible(false);
+	    	        	tutorial.setEnabled(true);
+	    		    	
+	    	        }  
+	    	    });
 	            // makes the grid
 	        	tutorial.addActionListener(new ActionListener(){ 
 	    	        public void actionPerformed(ActionEvent e){
@@ -65,54 +105,62 @@ public class Main {
 	    }); 
         
 	}
-	public static void sizeMenu(JFrame frame) {
-		
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();   
-		
-		JTextField selctorw = new JTextField("0");
-		JTextField selctorh = new JTextField("0");
-		JButton confirm = new JButton("Confirm size");
-		
-		selctorw.setBounds((int) screenSize.getWidth()/2 -50, (int) screenSize.getHeight()/2 -50, 100 ,100 );
-		selctorh.setBounds((int) screenSize.getWidth()/2 +50, (int) screenSize.getHeight()/2 -50, 100 ,100 );
-		
-		
-        // adding button in JFrame
-		confirm.setBounds((int) screenSize.getWidth()/2-50, (int) screenSize.getHeight()/2 +50, 200 ,100 );
-		frame.add(confirm);
-		confirm.doClick();
-		
-		
-        frame.add(selctorw);
-        frame.add(selctorh);
-        confirm.addActionListener(new ActionListener(){ 
-	        public void actionPerformed(ActionEvent e){  	
-	        	w = Integer.parseInt(selctorw.getText());
-	        	h = Integer.parseInt(selctorh.getText());
-	        	confirm.setVisible(false);
-	        	selctorw.setVisible(false);
-	        	selctorh.setVisible(false);
-	        }  
-	    });
-        
-       	
 
-
-	}
 	public static void girdMaker(JFrame frame) {
 		ArrayList<JButton> buttonList = new ArrayList<JButton>();
+	
+		ArrayList<Integer> cords = new ArrayList<Integer>();
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); 
-		for(int i =0; i<h; i++) {
-			for(int k =0; k<w; k++) {
+		
+		
+		JButton submit = new JButton("check word");
+		submit.setBounds((int) (screenSize.getWidth()*0.75),0, (int)screenSize.getWidth()/(w+1) ,(int) screenSize.getHeight()/(h+1) );
+		frame.add(submit);
+		submit.doClick();
+		
+		submit.addActionListener(new ActionListener(){ 
+	        public void actionPerformed(ActionEvent e){ 
+	        	for(int a =1; a<cords.size(); a++) {
+	        		if(cords.get(a) > cords.get(a+1)) {
+	        			 temp = cords.get(a);
+	        			 cords.set(a, a+1);
+	        			 cords.set(a+1, temp);
+	        			 
+	        		}
+	        	}
+	        	for(int a =0; a<cords.size(); a++) {
+	        		attempt+= buttonList.get(cords.get(a));
+
+	        	}
+        		if(secretWord ==attempt) {
+        			for(int c = 0; c<cords.size(); c++) {
+        				
+        			}
+        		}
+	        	
+	        }  
+	    }); 
+	        
+		for(i =0; i<h; i++) {
+			for( k =0; k<w; k++) {
 				JButton p = new JButton("a");
-				p.setBounds((int) screenSize.getWidth()/w *k, (int) screenSize.getHeight()/h *i, (int)screenSize.getWidth()/w ,(int) screenSize.getHeight()/h );
+				p.setBounds((int) screenSize.getWidth()/(w+1) *k, (int) screenSize.getHeight()/(h+1) *i, (int)screenSize.getWidth()/(w+1) ,(int) screenSize.getHeight()/(h+1) );
 				frame.add(p);
 				p.doClick();
-				p.setEnabled(false);
-				buttonList.add(p);
 				
+				buttonList.add(p);
+	            p.addActionListener(new ActionListener(){ 
+	    	        public void actionPerformed(ActionEvent e){ 
+	    	        
+	    	        	cords.add(i*w+k);
+	    	        	
+	    	        	p.setEnabled(false);
+	    	        }  
+	    	    }); 
+	    	        }
 			}
+
 		}
 
 	}
-}
+
